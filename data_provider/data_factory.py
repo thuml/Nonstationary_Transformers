76d6c809260a89aa52f76ad413ee890c3d71034d
Yong Liu <liuyong1095556447@gmail.com>
2023-06-16 17:09:06 +0800

    fix data_factory

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -16,8 +16,8 @@ def data_provider(args, flag):
 
     if flag == 'test':
         shuffle_flag = False
-        drop_last = True
-        batch_size = args.batch_size
+        drop_last = False
+        batch_size = 1
         freq = args.freq
     elif flag == 'pred':
         shuffle_flag = False
